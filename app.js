2ad098cd3986786277862765765f68b685b9238d
Sule <aybilenden@gmail.com>
2022-05-04 21:13:36 +0300

    done

--- app.js
+++ app.js
@@ -4,17 +4,33 @@ let calendar = document.getElementById("calendar");
 let deleteAll = document.getElementById("deleteAll");
 let list = document.querySelector("#list");
 
+inputText.focus();
+
+let oldTodo = JSON.parse(localStorage.getItem("todoItem"));
+window.addEventListener("load", () => {
+  console.log(oldTodo);
+  console.log(inputList);
+  oldTodo.forEach((element) => {
+    CreateItem(element);
+  });
+});
+
 const d = new Date();
 calendar.innerHTML += `${d.getUTCDate()}- ${
   d.getMonth() + 1
 }- ${d.getFullYear()}`;
 
-add.addEventListener("click", () => {
+let inputList = oldTodo ? oldTodo : [];
+add.addEventListener("click", (e) => {
+  e.preventDefault();
   if (inputText.value == "") {
     alert("Please Enter The Note ");
     return;
   } else {
     CreateItem(inputText.value);
+    inputList.push(inputText.value);
+    // console.log(inputList);
+    localStorage.setItem("todoItem", JSON.stringify(inputList));
     inputText.value = "";
   }
 });
@@ -24,16 +40,25 @@ function CreateItem(item) {
   let t = document.createTextNode(item); // text ekleyebilmek için
   li.className = "checked";
   li.appendChild(t);
-
+  
   list.appendChild(li);
   let span = document.createElement("span"); // x eklemek için
   let text = document.createTextNode("\u00D7"); // x ascii code
   span.className = "close";
   span.appendChild(text);
   li.appendChild(span);
-
+  
+  // console.log(li.innerText.slice(0, -1));
   span.addEventListener("click", () => {
     li.style.display = "none";
+    // silinecek item ı yakalamak için : 
+    let deleteItem = li.innerText.slice(0, -1)
+    // listeden item ı kaldırmak için : 
+    let index = inputList.indexOf(deleteItem);
+    inputList.splice(index,1)
+    // console.log(inputList);
+    //Son hali yeniden localStorage a gönderir:
+    localStorage.setItem('todoItem', JSON.stringify(inputList));
   });
 
   li.addEventListener("click", () => {
@@ -49,6 +74,13 @@ function CreateItem(item) {
   deleteAll.addEventListener("click", () => {
     if (li.style.textDecoration == "line-through") {
       li.style.display = "none";
+      let deleteItem = li.innerText.slice(0, -1)
+      // listeden item ı kaldırmak için : 
+      let index = inputList.indexOf(deleteItem);
+      inputList.splice(index,1)
+      // console.log(inputList);
+      //Son hali yeniden localStorage a gönderir:
+      localStorage.setItem('todoItem', JSON.stringify(inputList));
     }
   });
 }
